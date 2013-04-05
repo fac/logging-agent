35851d884b615ecdf7fd6b60a1b8d0e259060807
cornet <cornet@sheepy.org>
2013-04-05 11:30:42 +0100

    Parse out timestamp from barnyard log

--- lib/log_agent/filter/barnyard.rb
+++ lib/log_agent/filter/barnyard.rb
@@ -4,24 +4,26 @@ module LogAgent::Filter
     include LogAgent::LogHelper
 
     def << event
-      if event.message =~ /^[0-9\-:\.\/]+  \[\*\*\] \[([0-9]+):([0-9]+):([0-9]+)\] (.*) \[\*\*\] \[Classification(| ID): (.*)\] \[Priority(| ID): (.*?)\] {(.*?)} ([0-9\.:]+) -> ([0-9\.:]+)$/
-        event.fields['barnyard_gen_id']    = $1.to_i
-        event.fields['barnyard_sig_id']    = $2.to_i
-        event.fields['barnyard_sig_rev']   = $3.to_i
-        event.fields['barnyard_desc']      = $4
-        event.fields['barnyard_class']     = $6
-        event.fields['barnyard_priority']  = $8.to_i
-        event.fields['barnyard_proto']     = $9
-        event.fields['barnyard_src_ip']    = $10.split(':')[0]
-        event.fields['barnyard_src_port']  = $10.split(':')[1] ? $10.split(':')[1].to_i : nil
-        event.fields['barnyard_dest_ip']   = $11.split(':')[0]
-        event.fields['barnyard_dest_port'] = $11.split(':')[1] ? $11.split(':')[1].to_i : nil
+      if event.message =~ /^([0-9\-:\.\/]+)  \[\*\*\] \[([0-9]+):([0-9]+):([0-9]+)\] (.*) \[\*\*\] \[Classification(| ID): (.*)\] \[Priority(| ID): (.*?)\] {(.*?)} ([0-9\.:]+) -> ([0-9\.:]+)$/
+        event.timestamp = Time.parse($1) rescue Time.now
+        event.fields['barnyard_gen_id']    = $2.to_i
+        event.fields['barnyard_sig_id']    = $3.to_i
+        event.fields['barnyard_sig_rev']   = $4.to_i
+        event.fields['barnyard_desc']      = $5
+        event.fields['barnyard_class']     = $7
+        event.fields['barnyard_priority']  = $9.to_i
+        event.fields['barnyard_proto']     = $10
+        event.fields['barnyard_src_ip']    = $11.split(':')[0]
+        event.fields['barnyard_src_port']  = $11.split(':')[1] ? $11.split(':')[1].to_i : nil
+        event.fields['barnyard_dest_ip']   = $12.split(':')[0]
+        event.fields['barnyard_dest_port'] = $12.split(':')[1] ? $12.split(':')[1].to_i : nil
 
-      elsif event.message =~ /^[0-9\-:\.\/]+  \[\*\*\] \[([0-9]+):([0-9]+):([0-9]+)\] (.*) \[\*\*\] $/
-        event.fields['barnyard_gen_id']  = $1.to_i
-        event.fields['barnyard_sig_id']  = $2.to_i
-        event.fields['barnyard_sig_rev'] = $3.to_i
-        event.fields['barnyard_desc']    = $4
+      elsif event.message =~ /^([0-9\-:\.\/]+)  \[\*\*\] \[([0-9]+):([0-9]+):([0-9]+)\] (.*) \[\*\*\] $/
+        event.timestamp = Time.parse($1) rescue Time.now
+        event.fields['barnyard_gen_id']  = $2.to_i
+        event.fields['barnyard_sig_id']  = $3.to_i
+        event.fields['barnyard_sig_rev'] = $4.to_i
+        event.fields['barnyard_desc']    = $5
       end
 
       emit event
--- spec/functional/filter/barnyard_spec.rb
+++ spec/functional/filter/barnyard_spec.rb
@@ -27,6 +27,17 @@ describe LogAgent::Filter::Barnyard do
       filter << entry1
     end
     
+    it 'should parse the timestamp correctly' do
+      entry1.timestamp.to_s.should =~ /#{Time.now.year}-03-22 12:43:42 \+\d\d\d\d/
+      entry2.timestamp.to_s.should =~ /#{Time.now.year}-03-22 12:43:34 \+\d\d\d\d/
+      entry3.timestamp.to_s.should =~ /#{Time.now.year}-04-02 11:18:04 \+\d\d\d\d/
+      entry5.timestamp.to_s.should =~ /#{Time.now.year}-03-22 01:24:25 \+\d\d\d\d/
+    end
+
+    it 'should use the current time if log timestamp is invalid' do
+      entry4.timestamp.should be_within(1).of(Time.now)
+    end
+
     it 'should parse the generator id' do
       entry1.fields['barnyard_gen_id'].should == 129
       entry2.fields['barnyard_gen_id'].should == 3
