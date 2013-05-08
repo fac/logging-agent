73a20d416cbd16d75d4eb8eaedcfa685b684d3b8
cornet <cornet@sheepy.org>
2013-05-08 16:30:07 +0100

    Make sure nothing goes wrong if parsing fails

--- lib/log_agent/filter/pt_deadlock.rb
+++ lib/log_agent/filter/pt_deadlock.rb
@@ -5,34 +5,38 @@ module LogAgent::Filter
     include LogAgent::LogHelper
 
     def << event
-      lines = event.message.split("\n")
-      lines.shift
+      begin
+        lines = event.message.split("\n")
+        lines.shift
 
 
-      lines.each_index do |i|
-        tx = lines[i].split(/ /, 16)
+        lines.each_index do |i|
+          tx = lines[i].split(/ /, 16)
 
-        # Timestamps are always the same for each tx
-        event.timestamp = event.timestamp = Time.parse(tx[1])
+          # Timestamps are always the same for each tx
+          event.timestamp = event.timestamp = Time.parse(tx[1])
 
-        event.fields["tx#{i+1}"] = {}
-        event.fields["tx#{i+1}"]['server']    = tx[0]
-        event.fields["tx#{i+1}"]['thread']    = tx[2].to_i
-        event.fields["tx#{i+1}"]['txn_id']    = tx[3].to_i
-        event.fields["tx#{i+1}"]['txn_time']  = tx[4].to_i
-        event.fields["tx#{i+1}"]['user']      = tx[5]
-        event.fields["tx#{i+1}"]['hostname']  = tx[6]
-        event.fields["tx#{i+1}"]['ip']        = tx[7]
-        event.fields["tx#{i+1}"]['db']        = tx[8]
-        event.fields["tx#{i+1}"]['tbl']       = tx[9]
-        event.fields["tx#{i+1}"]['idx']       = tx[10]
-        event.fields["tx#{i+1}"]['lock_type'] = tx[11]
-        event.fields["tx#{i+1}"]['lock_mode'] = tx[12]
-        event.fields["tx#{i+1}"]['wait_hold'] = tx[13]
-        event.fields["tx#{i+1}"]['victim']    = tx[14].to_i
-        event.fields["tx#{i+1}"]['query']     = tx[15]
+          event.fields["tx#{i+1}"] = {}
+          event.fields["tx#{i+1}"]['server']    = tx[0]
+          event.fields["tx#{i+1}"]['thread']    = tx[2].to_i
+          event.fields["tx#{i+1}"]['txn_id']    = tx[3].to_i
+          event.fields["tx#{i+1}"]['txn_time']  = tx[4].to_i
+          event.fields["tx#{i+1}"]['user']      = tx[5]
+          event.fields["tx#{i+1}"]['hostname']  = tx[6]
+          event.fields["tx#{i+1}"]['ip']        = tx[7]
+          event.fields["tx#{i+1}"]['db']        = tx[8]
+          event.fields["tx#{i+1}"]['tbl']       = tx[9]
+          event.fields["tx#{i+1}"]['idx']       = tx[10]
+          event.fields["tx#{i+1}"]['lock_type'] = tx[11]
+          event.fields["tx#{i+1}"]['lock_mode'] = tx[12]
+          event.fields["tx#{i+1}"]['wait_hold'] = tx[13]
+          event.fields["tx#{i+1}"]['victim']    = tx[14].to_i
+          event.fields["tx#{i+1}"]['query']     = tx[15]
+        end
+
+      rescue
+        # In case something goes south with the parsing
       end
-
       emit event
     end
   end
