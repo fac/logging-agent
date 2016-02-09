49c3feb5fbf78d0b37786e0ef157598a97df3629
Steven Williamson <steve@freeagent.com>
2016-02-09 10:04:36 +0000

    Make message persistence configurable
    - Default to making messages persistent but allow it to be turned off
      via configuration.

--- lib/log_agent/output/amqp.rb
+++ lib/log_agent/output/amqp.rb
@@ -1,15 +1,25 @@
 module LogAgent::Output
   class AMQP
     include LogAgent::LogHelper
-    
-    attr_reader :channel, :exchange
-    def initialize channel, exchange
-      @channel, @exchange = channel, exchange
+
+    attr_reader :channel, :exchange, :persistent_msgs
+    # Initialise AMQP output module
+    #
+    # * channel - Bunny channle
+    # * exchange - Bunny exchange
+    # * persistent_msgs - Should messages survive a broker restart
+    #   persistent_msgs defaults to true.
+    def initialize(channel, exchange, persistent_msgs = true)
+      @channel, @exchange, @persistent_msgs = channel, exchange, persistent_msgs
     end
-    
+
     def << event
       debug "Shipping event '#{event.uuid}'"
-      @exchange.publish(event.to_payload, :routing_key => "#{event.type}.#{event.source_host}", :persistent => true) do
+      @exchange.publish(
+        event.to_payload,
+        :routing_key => "#{event.type}.#{event.source_host}",
+        :persistent  => persistent_msgs
+      ) do
         yield if block_given?
       end
     end
