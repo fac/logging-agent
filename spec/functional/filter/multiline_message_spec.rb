32fee72627bd0d3d0c24b5ef7f077a205cdcbc5c
Donal McBreen <donal@freeagent.com>
2020-05-01 14:27:48 +0100

    Include op_id when reducing events

--- spec/functional/filter/multiline_message_spec.rb
+++ spec/functional/filter/multiline_message_spec.rb
@@ -2,45 +2,45 @@ require 'spec_helper'
 
 describe LogAgent::Filter::MultilineMessage do
   let(:sink) { mock("MySink", :<< => nil) }
-  
+
   it "should init with new( sink, options )" do
     filter = LogAgent::Filter::MultilineMessage.new sink, {:options => true}
     filter.sink.should == [sink]
     filter.options[:options].should be_true
   end
-  
+
   describe "in :start => /regexp/, :end => /regexp/ mode" do
     let(:test_timestamp) { Time.now }
-    let(:start_line) { LogAgent::Event.new :message => "START OF BLOCK", :type => "railz", :uuid => 'foo', :tags => ['tag1', 'tag2'], :timestamp => test_timestamp, :source_type => "source_type", :source_path => "source_path" }
+    let(:start_line) { LogAgent::Event.new :message => "START OF BLOCK", :type => "railz", :uuid => 'foo', :tags => ['tag1', 'tag2'], :timestamp => test_timestamp, :source_type => "source_type", :source_path => "source_path", :op_id => "1234"}
     let(:end_line) { LogAgent::Event.new :message => "END OF BLOCK" }
     let(:line1) { LogAgent::Event.new :message => "extra line 1", :tags => ['another tag'] }
     let(:line2) { LogAgent::Event.new :message => "extra line 2" }
-    
+
     let(:filter) { LogAgent::Filter::MultilineMessage.new sink, {:start => /^START/, :end => /^END/ } }
-    
+
     it "should emit a single event for each event when not in a buffered block" do
       sink.should_receive(:<<).ordered.with(line1)
       sink.should_receive(:<<).ordered.with(line2)
       filter << line1
       filter << line2
     end
-    
+
     describe "when the start token is seen" do
       before { filter << start_line }
-      
+
       it "buffer all subsequent events" do
         sink.should_not_receive(:<<)
         filter << line1
         filter << line2
       end
-    
+
       it "should emit a single event when the end regexp is matched" do
         sink.should_receive(:<<).exactly(1).times.with(an_instance_of(LogAgent::Event))
         filter << line1
         filter << line2
         filter << end_line
       end
-      
+
       it "should emit a message with the same UUID, timestamp, source_*, tags and type as the start message" do
         sink.should_receive(:<<) { |event|
           event.uuid.should == 'foo'
@@ -50,6 +50,7 @@ describe LogAgent::Filter::MultilineMessage do
           event.timestamp.should == test_timestamp
           event.type.should == 'railz'
           event.tags.should == ['tag1', 'tag2']
+          event.op_id.should == "1234"
         }
         filter << line1
         filter << line2
@@ -69,11 +70,11 @@ describe LogAgent::Filter::MultilineMessage do
         filter << line1
         filter << line2
         filter << end_line
-        filter << line1        
+        filter << line1
       end
     end
-    
+
   end
-  
-  
+
+
 end
